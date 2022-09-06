72579fd8fd92f58e1ba2f433824fd0bed70bb767
pyuung <kyuungah@gmail.com>
2022-09-07 02:46:18 +0900

    feat: :sparkles: 로그인 기본 로직 구현

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,33 @@
+import * as bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
+import { jwtToken } from '../../common/util/jwt'
+import { User } from '../user/user.schema'
 
 export class AuthService {
   constructor() {}
 
-  login(req: Request, res: Response) {
+  async login(req: Request, res: Response) {
     try {
-      const data = req.body
-      console.log(req.body)
-      res.status(200).send({ success: 'data' })
+      const { email, key } = req.body
+      const user = await User.findOne({ email })
+      if (!user) return res.send({ message: '이메일을 확인해주세요.' })
+
+      const isPasswordValidated = await bcrypt.compare(key, user.key)
+      if (!isPasswordValidated) return res.send({ message: '비밀번호를 확인해주세요.' })
+
+      const token = jwtToken.createToken(email)
+
+      return res.send({
+        success: {
+          statusCode: 200,
+          json: {
+            status: 'ok',
+            data: {
+              token,
+            },
+          },
+        },
+      })
     } catch (error: any) {
       res.status(400).send(error)
     }
